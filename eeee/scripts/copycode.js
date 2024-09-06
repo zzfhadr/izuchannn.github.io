c5717cf0dcde9f59a83c918ceb466de159331096
unknown <zzfhadr@gmail.com>
2024-09-06 14:05:12 +0800

    autobatscriptdoupdateforme

--- eeee/scripts/copycode.js
+++ eeee/scripts/copycode.js
@@ -1,14 +1,11 @@
 // This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll('.language-plaintext highlighter-rouge');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
-
+const codeBlocks = document.querySelectorAll('.language-plaintext.highlighter-rouge');
+const copyCodeButtons = document.querySelectorAll('.copyButton');
 copyCodeButtons.forEach((copyCodeButton, index) => {
   const code = codeBlocks[index].innerText;
-
   copyCodeButton.addEventListener('click', () => {
     // Copy the code to the user's clipboard
     window.navigator.clipboard.writeText(code);
-
     // Update the button text visually
     const { innerText: originalText } = copyCodeButton;
     copyCodeButton.innerText = 'Copied!';
