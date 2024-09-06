f8f977e8ea6273113c8db50aa0ae002fbd55fe31
unknown <zzfhadr@gmail.com>
2024-09-06 08:58:12 +0800

    autobatscriptdoupdateforme

--- _posts/new.bat
+++ _posts/new.bat
@@ -5,11 +5,11 @@ SET path23=%%F
 )
 for /f "tokens=1,2* delims= " %%i in  ('echo %date:/=-%') do ( set abasd=%%j %%i)
 set middeel=%abasd% %time::=：%
-echo --- >> "%path23%\%middeel:.=：%-%1.markup"
-echo layout: post >> "%path23%\%middeel:.=：%-%1.markup"
-echo title: "%1" >> "%path23%\%middeel:.=：%-%1.markup"
-echo --- >> "%path23%\%middeel:.=：%-%1.markup"
-echo # %middeel:.=：% >>  "%path23%\%middeel:.=：%-%1.markup"
+echo --- >> "%path23%\%middeel:.=：%-%1.md"
+echo layout: post >> "%path23%\%middeel:.=：%-%1.md"
+echo title: "%1" >> "%path23%\%middeel:.=：%-%1.md"
+echo --- >> "%path23%\%middeel:.=：%-%1.md"
+echo # %middeel:.=：% >>  "%path23%\%middeel:.=：%-%1.md"
 
-@rem "F:\Microsoft VS Code\Code.exe" "%middeel:.=：%-%1.markup"
-"F:\Sublime Text\sublime_text.exe" "%middeel:.=：%-%1.markup"
+@rem "F:\Microsoft VS Code\Code.exe" "%middeel:.=：%-%1.md"
+"F:\Sublime Text\sublime_text.exe" "%middeel:.=：%-%1.md"
