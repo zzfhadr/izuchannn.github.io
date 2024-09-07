082ee94d5010061893111fef16f0b3d2617f02cf
unknown <zzfhadr@gmail.com>
2024-09-07 10:22:14 +0800

    autobatscriptdoupdateforme

--- _posts/new.bat
+++ _posts/new.bat
@@ -3,7 +3,7 @@ chcp 65001
 FOR /F "tokens=* USEBACKQ" %%F IN (`cd`) DO (
 SET path23=%%F
 )
-
+for /f "tokens=1,2* delims= " %%i in  ('echo %date:/=-%') do ( set abasd=%%j-%%i)
 set middeel=%abasd: =%-%time::=：%
 echo --- >> "%path23%\%middeel:.=：%-%1.md"
 echo layout: post >> "%path23%\%middeel:.=：%-%1.md"
